2fc96efc184e7675b7f18cb8d1a2df15573f46bb
Kevin Bates <kbates4@gmail.com>
2019-08-01 11:46:50 -0700

    Remove container-based launchers
    The container-based launchers for kubernetes and docker will be hosted
    by their respective kernel providers since they contain scripts relative
    to those providers.  The actualy kernel launchers will remain here since
    they are common to all providers (at some point).
    
    With this change, the spec utilities were updated to allow for only
    resource files (icons, etc.) to be copied since the container-based
    launchers will position their pod/container launchers appropriately.
    
    This will trigger a 0.2.0 release of Remote Kernel Provider on which
    the container-based providers will minimally depend.

--- remote_kernel_provider/spec_utils.py
+++ remote_kernel_provider/spec_utils.py
@@ -7,7 +7,7 @@ from distutils import dir_util
 kernel_launchers_dir = path.join(path.dirname(__file__), 'kernel-launchers')
 kernel_resources_dir = path.join(path.dirname(__file__), 'kernel-resources')
 
-launcher_dirs = ['python', 'r', 'scala', 'kubernetes', 'docker']
+launcher_dirs = ['python', 'r', 'scala']
 resource_dirs = ['python', 'r', 'scala', 'tensorflow']
 
 
@@ -23,10 +23,13 @@ def delete_staging_directory(dir_name):
     shutil.rmtree(dir_name)
 
 
-def copy_kernelspec_files(dir_name, launcher_type='python', resource_type=None):
+def copy_kernelspec_files(dir_name, launcher_type=None, resource_type=None):
     """Copies the launcher files specified by `launcher_type` to the specified directory."""
 
-    if launcher_type not in launcher_dirs:
+    if launcher_type is None and resource_type is None:
+        raise ValueError("Invalid parameters.  At least one of 'launcher_type' or 'resource_type' must be specified!")
+
+    if launcher_type is not None and launcher_type not in launcher_dirs:
         raise ValueError("Invalid launcher_type '{}' detected! Must be one of: {}".format(launcher_type, launcher_dirs))
 
     # If resource is not specified, default to launcher_type.  For the most part it will be the language,
@@ -36,8 +39,10 @@ def copy_kernelspec_files(dir_name, launcher_type='python', resource_type=None):
     if resource_type not in resource_dirs:
         raise ValueError("Invalid resource_type '{}' detected! Must be one of: {}".format(resource_type, resource_dirs))
 
-    src_dir = path.join(kernel_launchers_dir, launcher_type)
-    dir_util.copy_tree(src=src_dir, dst=dir_name)
+    if launcher_type is not None:
+        src_dir = path.join(kernel_launchers_dir, launcher_type)
+        dir_util.copy_tree(src=src_dir, dst=dir_name)
 
-    src_dir = path.join(kernel_resources_dir, resource_type)
-    dir_util.copy_tree(src=src_dir, dst=dir_name)
+    if resource_type is not None:
+        src_dir = path.join(kernel_resources_dir, resource_type)
+        dir_util.copy_tree(src=src_dir, dst=dir_name)
